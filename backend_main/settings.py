9e3f69b59c1029c16ef806d57c1c885818760473
seems-developer <seems.developer@gmail.com>
2026-03-05 22:05:46 +0530

    finally again

--- backend_main/settings.py
+++ backend_main/settings.py
@@ -73,9 +73,10 @@ CORS_ALLOWED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL","sqlite:///db.sqlite3")
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
